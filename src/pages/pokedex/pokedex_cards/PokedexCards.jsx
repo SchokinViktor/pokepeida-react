7bc57a298649ef596067ba9baf31987a9ac45ddb
Viktor <viktor.shchokin.a@gmail.com>
2022-07-30 15:45:07 +0300

    changed regular pagination to react pagination libr

--- src/pages/pokedex/pokedex_cards/PokedexCards.jsx
+++ src/pages/pokedex/pokedex_cards/PokedexCards.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import ReactPaginate from "react-paginate";
 
-import Pagination from "../../../components/pagination/Pagination";
 // import PokemonCard from "../../../components/pokedex_card/PokedexCard";
 import PokemonsList from "./PokemonsList";
-
 
 const PokedexCards = ({ pokemonData, setPokemonData }) => {
   const [isLoading, setLoading] = useState(true);
 
-  const [url, setUrl] = useState(
-    `https://pokeapi.co/api/v2/pokemon/?limit=151`
-  );
+  const url = `https://pokeapi.co/api/v2/pokemon/?limit=151`;
 
   const getPokemons = async (data) => {
     data.map(async (item) => {
@@ -27,20 +24,39 @@ const PokedexCards = ({ pokemonData, setPokemonData }) => {
     });
   };
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const cardsPerPage = 12;
-  const lastCardIndex = currentPage * cardsPerPage;
-  const firstCardIndex = lastCardIndex - cardsPerPage;
-  const currentPokemons = pokemonData.slice(firstCardIndex, lastCardIndex);
-  const paginate = (pageNumber) => {
-    setCurrentPage(pageNumber);
+  const [pageRange, setPageRange] = useState(5);
+  const [marginsPages, setMarginsPages] = useState(1);
+
+  const changePaginationRange = () => {
+    const {innerWidth: windowWidth} = window;
+    if (windowWidth < 768) {
+      setPageRange(2)
+    } 
+  }
+
+  
+
+  const [currentItems, setCurrentItems] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0); // index of the first item in currentp page
+  const itemsPerPage = 12;
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage; // index of the last item in current page
+    setCurrentItems(pokemonData.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(pokemonData.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, pokemonData]);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % pokemonData.length;
+    setItemOffset(newOffset);
   };
 
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
       const response = await axios.get(url);
-      console.log(response.data);
+      //console.log(response.data);
       getPokemons(response.data.results);
       setLoading(false);
     };
@@ -50,7 +66,7 @@ const PokedexCards = ({ pokemonData, setPokemonData }) => {
   return (
     <section className='pokedex-cards'>
       <div className='container'>
-        <PokemonsList pokemonData={currentPokemons} isLoading = {isLoading} />
+        <PokemonsList pokemonData={currentItems} isLoading={isLoading} />
         {/* <ul className='pokedex-cards__cards-list'>
           {currentPokemons.map((item, i) => {
             return (
@@ -60,16 +76,25 @@ const PokedexCards = ({ pokemonData, setPokemonData }) => {
             );
           })}
         </ul> */}
-        
-        <Pagination
-          elementsPerPage={cardsPerPage}
-          totalElements={pokemonData.length}
-          OnChange={paginate}
-          currentPage={currentPage}
-          buttons={true}
-        />
 
-       
+        <ReactPaginate
+          breakLabel='...'
+          nextLabel='>'
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={pageRange}
+          marginPagesDisplayed = {1}
+          pageCount={pageCount}
+          previousLabel='<'
+          renderOnZeroPageCount={null}
+          containerClassName = 'pokedex-pagination'
+          pageClassName = 'pokedex-pagination__list-item'
+          activeClassName = ''
+          pageLinkClassName = 'pokedex-pagination__pagination-page'
+          activeLinkClassName = 'pokedex-pagination__pagination-page_active'
+          breakLinkClassName = 'pokedex-pagination__pagination-page break-link'
+          previousLinkClassName = 'pokedex-pagination__pagination-btn'
+          nextLinkClassName = 'pokedex-pagination__pagination-btn'
+        />
       </div>
     </section>
   );
